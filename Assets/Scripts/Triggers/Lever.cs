fd1c44580c7165f5fc0e65e1fac6053878af83c1
chomash <thomas.czuma@gmail.com>
2023-02-05 06:09:41 +0100

    lever now toggles

--- Assets/Scripts/Triggers/Lever.cs
+++ Assets/Scripts/Triggers/Lever.cs
@@ -6,10 +6,12 @@ public class Lever : Trigger
 { 
     //private Movable moveThis;
    [SerializeField]private MovableObject objectToMove;
+    [SerializeField] private SpriteRenderer lever;
     protected override void DoTrigger()
     {
         if (Input.GetAxisRaw("Interact") == 1)
         {
+            lever.flipX = true;
             objectToMove.Trigger();
         }
     }
