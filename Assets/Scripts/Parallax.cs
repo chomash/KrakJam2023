d64cb78ad1f6818ca6f56b5c1a6dd2ebdf3bd1f7
EnzerMen <61794871+EnzerMen@users.noreply.github.com>
2023-02-05 01:34:11 +0100

    ttt

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    #region Variables
+
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    #endregion
+
+
+    void Start()
+    {
+        startpos = transform.position.y;
+        //length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.y * (1 - parallaxEffect));
+        float dist = (cam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(transform.position.x, startpos + dist + 3f,  transform.position.z);
+
+        // if (temp > startpos + length) startpos += length;
+        // else if (temp < startpos - length) startpos -= length;
+    }
+}
