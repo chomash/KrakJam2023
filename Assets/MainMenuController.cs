adb95e4f79fc7a844caa9679abf3e837b4d8dd81
chomash <thomas.czuma@gmail.com>
2023-02-07 18:44:36 +0100

    animated main menu, and main menu scene/controller fix
    also typo in the dialogue

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -6,26 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject mainBox, creditBox;
-    private void Awake()
-    {
-        mainBox.SetActive(true);
-        creditBox.SetActive(false);
-    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Solarpunk");
-    }
-
-    public void CreditsIn()
-    {
-        mainBox.SetActive(false);
-        creditBox.SetActive(true);
-    }
-
-    public void CreditsOut()
-    {
-        mainBox.SetActive(true);
-        creditBox.SetActive(false);
     }
 
     public void EndGame()
